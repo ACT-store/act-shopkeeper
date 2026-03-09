574664730fa9734b16922423b5fe014f13c311f4
ACT-store <actneiua@gmail.com>
2026-03-10 01:15:20 +1100

    Fix: green login bg, real Firebase config, proper app icons

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -4,12 +4,12 @@ import { getAuth, setPersistence, browserLocalPersistence } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "REPLACE_WITH_ACT_STORE_API_KEY",
+  apiKey: "AIzaSyAhvyvlyW98LfmwECcqTb9-chRuEc4cAEc",
   authDomain: "act-store-b377b.firebaseapp.com",
   projectId: "act-store-b377b",
-  storageBucket: "act-store-b377b.appspot.com",
-  messagingSenderId: "REPLACE_WITH_ACT_STORE_SENDER_ID",
-  appId: "REPLACE_WITH_ACT_STORE_APP_ID"
+  storageBucket: "act-store-b377b.firebasestorage.app",
+  messagingSenderId: "744358651817",
+  appId: "1:744358651817:web:35d5d8f63d5325580946b8"
 };
 
 // Initialize Firebase
